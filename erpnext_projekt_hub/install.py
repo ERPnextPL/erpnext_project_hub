d2aa0bf527aec4747436758f5b72db17abfad3c1
sliwkawsieci <sliwkipoczta@gmail.com>
2025-12-10 17:48:12 +0100

    poprawa instalacji

--- erpnext_projekt_hub/install.py
+++ erpnext_projekt_hub/install.py
@@ -4,7 +4,8 @@ def before_install():
     """
     Sprawdź, czy erpnext jest zainstalowane przed instalacją projekt_hub
     """
-    if not frappe.db.exists("App", "erpnext"): 
+    installed_apps = frappe.get_installed_apps()
+    if "erpnext" not in installed_apps:
         frappe.throw("Aplikacja erpnext jest wymagana do zainstalowania Projekt HUB. Proszę zainstalować erpnext najpierw.")
 
 def after_install():
