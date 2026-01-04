7e8995e08eb3463ed49d4a06c26c4f54d43086a7
sliwkawsieci <sliwkipoczta@gmail.com>
2026-01-04 22:35:42 +0100

    poprawa instalac ji i after install

--- erpnext_projekt_hub/install.py
+++ erpnext_projekt_hub/install.py
@@ -9,3 +9,8 @@ def before_install():
 	installed_apps = frappe.get_installed_apps()
 	if "erpnext" not in installed_apps:
 		frappe.throw(_("The erpnext app is required to install Projekt HUB. Please install erpnext first."))
+
+
+def after_install():
+	"""Placeholder hook for any post-install setup the app requires."""
+	pass
