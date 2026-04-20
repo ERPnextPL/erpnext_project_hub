b043cc46dd5019078d5ffae8d031d5bddc7adea7
sliwkawsieci <sliwkipoczta@gmail.com>
2026-04-20 08:15:12 +0200

    przywrócenie wersji taskdetails z develop

---
+++ erpnext_projekt_hub/public/frontend/assets/chunks/InsertVideo-aadcac0b.js
@@ -0,0 +1,2 @@
+import{ai as r,af as s,ag as t,ah as d}from"./frappe-ui-ba113b18.js";const p=r({__name:"InsertVideo",props:{editor:{}},setup(e){const o=e;function a(){o.editor.chain().focus().selectAndUploadVideo().run()}return(n,i)=>s(n.$slots,"default",t(d({onClick:a})))}});export{p as default};
+//# sourceMappingURL=InsertVideo-aadcac0b.js.map
